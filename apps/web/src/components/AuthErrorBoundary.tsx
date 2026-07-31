e43e89481b4184edbf2ab114da2e40b0aff3441f
Ashrafi <Mahdi.Ashrafi@gov.bc.ca>
2026-07-31 16:03:33 -0700

    Fixed a type import

--- apps/web/src/components/AuthErrorBoundary.tsx
+++ apps/web/src/components/AuthErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from "react";
+import { Component, type ReactNode } from "react";
 import { ForbiddenError } from "../api/client"; // adjust path
 import { AccessDenied } from "./AccessDenied";
 
